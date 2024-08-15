de750cd51b29a9f9bedfe677d912dbbde3d4fdcb
3papa3 <lopapa091@gmail.com>
2024-08-15 12:33:26 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "221765137968"
+global.ownername = "🐛 MR PAPA "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
